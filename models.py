b5c5db10dbf05948a69400e3257b6369205a40f8
KAMYAR <kazizzad@uci.edu>
2019-07-28 16:01:49 -0700

    bdqn-raw version

--- models.py
+++ models.py
@@ -4,6 +4,35 @@ import torch
 import torch.nn as nn
 
 
+class Deep_feature(nn.Module):
+    def __init__(self, input_shape, feature_dimension, num_actions):
+        super(Deep_feature, self).__init__()
+        self.input_shape = input_shape
+        self.num_actions = num_actions
+        self.features = nn.Sequential(
+            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU(),
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(self.feature_size(), feature_dimension),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
+    def feature_size(self):
+        return self.features(torch.zeros(1, *self.input_shape)).view(1, -1).size(1)
+
+
+
 class CnnDQN(nn.Module):
     def __init__(self, input_shape, num_actions):
         super(CnnDQN, self).__init__()
